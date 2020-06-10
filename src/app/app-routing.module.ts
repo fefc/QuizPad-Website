af811cda67146cb9d4d79c2e9a4869f4e96dba4c
Felix Chantrel <felchantrel@gmail.com>
2020-06-10 11:55:04 +0200

    Added pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'download',
-    pathMatch: 'full'
+    redirectTo: 'home',
+    pathMatch: 'prefix'
   },
-  /*{
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },*/
   {
     path: 'download',
     loadChildren: () => import('./pages/download/download.module').then( m => m.DownloadPageModule)
+  },
+  {
+    path: 'features',
+    loadChildren: () => import('./pages/features/features.module').then( m => m.FeaturesPageModule)
+  },
+  {
+    path: 'help',
+    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
